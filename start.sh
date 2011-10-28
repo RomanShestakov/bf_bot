29876cf774cd94c07ffe2a6d338cfe34c0d73e6a
Roman Shestakov <romanshestakov@yahoo.co.uk>
2011-10-29 00:17:48 +0100

    add params to be passed to application

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 cd `dirname $0`
-exec erl -pa $PWD/ebin -pa $PWD/deps/*/ebin -name bf_bot@127.0.0.1 \
-    -setcookie rs -s bf_bot_app start
+exec erl -pa $PWD/ebin -pa $PWD/deps/*/ebin -name bf_bot@127.0.0.1 -setcookie rs -s bf_bot_app -bf_bot marketId 102873784 
