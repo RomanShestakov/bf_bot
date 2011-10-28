7fcd2ccacc3841bbdf39f033ed4f25ac2365f29b
Roman Shestakov <romanshestakov@yahoo.co.uk>
2011-10-29 00:32:11 +0100

    pass marketid as param

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+# need an argument, MarketId , e.g.102873784
 cd `dirname $0`
-exec erl -pa $PWD/ebin -pa $PWD/deps/*/ebin -name bf_bot@127.0.0.1 -setcookie rs -s bf_bot_app -bf_bot marketId 102873784 
+exec erl -pa $PWD/ebin -pa $PWD/deps/*/ebin -name bf_bot@127.0.0.1 -setcookie rs -s bf_bot_app -bf_bot marketId $1 
